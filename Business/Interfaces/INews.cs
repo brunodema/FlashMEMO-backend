a54034aea440923d543773bf82d67450af67945f
Bruno Demantova <brunoeckdema@gmail.com>
2021-04-28 00:37:08 -0300

    add interface

---
+++ Business/Interfaces/INews.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface INews
+    {
+        public Guid NewsID { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ Data/Models/News.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Business.Interfaces;
+
+namespace Data.Models
+{
+    public class News : INews
+    {
+        public News() { }
+        public Guid NewsID { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
