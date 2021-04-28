911a5a5f9346e1b92c6d203954c6cd673eed3de6
Bruno Demantova <brunoeckdema@gmail.com>
2021-04-28 00:38:21 -0300

    add correct EF package

---
+++ Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using Data.Models;
+using System.Data.Entity;
+using System;
+
+namespace Data
+{
+    public class FlashMEMOContext : DbContext
+    {
+        public FlashMEMOContext() : base()
+        { 
+        }
+        public DbSet<News> News { get; set; }
+    }
+}
