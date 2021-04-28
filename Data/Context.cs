48762ae4927a70d4933b2825b29d259fbe9f02b2
Bruno Demantova <brunoeckdema@gmail.com>
2021-04-28 12:43:14 -0300

    add vscode stuff and project reference

--- Data/Context.cs
+++ Data/Context.cs
@@ -12,3 +12,6 @@ namespace Data
         public DbSet<News> News { get; set; }
     }
 }
+
+//https://balta.io/artigos/aspnet-5-autenticacao-autorizacao-bearer-jwt
+//https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/workflows/new-database#1-create-the-application
