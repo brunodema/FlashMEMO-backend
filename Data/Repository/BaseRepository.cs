0b03164fade32146784b2a1d5e4b04a862f72cc1
Bruno Demantova <brunoeckdema@gmail.com>
2021-05-12 21:36:40 -0300

    isolate interface from implementation

---
+++ Data/Repository/BaseRepository.cs
@@ -0,0 +1,73 @@
+﻿using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public abstract class BaseRepository<TEntity, DatabaseContext> : IBaseRepository<TEntity, DatabaseContext>
+        where TEntity : class
+        where DatabaseContext : DbContext
+    {
+        protected readonly DatabaseContext _context;
+        protected readonly DbSet<TEntity> _dbset;
+
+        protected BaseRepository(DatabaseContext context)
+        {
+            _context = context;
+            _dbset = context.Set<TEntity>();
+        }
+        public virtual async Task<IEnumerable<TEntity>> SearchAndOrderAsync<TKey>(Expression<Func<TEntity, bool>> predicate, SortType sortType, Expression<Func<TEntity, TKey>> columnToSort, int numRecords)
+        {
+            if (sortType == SortType.Ascending)
+            {
+                return await _dbset.AsNoTracking().Where(predicate).OrderBy(columnToSort).Take(numRecords).ToListAsync();
+            }
+            return await _dbset.AsNoTracking().Where(predicate).OrderByDescending(columnToSort).Take(numRecords).ToListAsync();
+
+        }
+        public virtual async Task<IEnumerable<TEntity>> SearchAllAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbset.AsNoTracking().Where(predicate).ToListAsync();
+        }
+        public virtual async Task<TEntity> SearchFirstAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbset.AsNoTracking().FirstOrDefaultAsync(predicate);
+        }
+        public virtual async Task<ICollection<TEntity>> GetAllAsync()
+        {
+            return await _dbset.ToListAsync();
+        }
+        public virtual async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await _dbset.FindAsync(id);
+        }
+        // CRUD
+        public virtual async Task CreateAsync(TEntity entity)
+        {
+            _dbset.Add(entity);
+            await SaveChangesAsync();
+        }
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            _dbset.Update(entity);
+            await SaveChangesAsync();
+        }
+        public virtual async Task RemoveAsync(TEntity entity)
+        {
+            _dbset.Remove(entity);
+            await SaveChangesAsync();
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+        public virtual void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
