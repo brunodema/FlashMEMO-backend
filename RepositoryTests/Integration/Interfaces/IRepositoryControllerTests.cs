68ed3d8eac734db840a81a582dc47b75f14e1e40
Bruno Demantova <brunoeckdema@gmail.com>
2021-06-06 20:55:26 -0300

    improve interfaces

--- RepositoryTests/Integration/Interfaces/IRepositoryControllerTests.cs
+++ RepositoryTests/Integration/Interfaces/IRepositoryControllerTests.cs
@@ -1,33 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests.Integration.Interfaces
 {
-    public interface IRepositoryControllerTests : IRepositoryControllerCreateTests, IRepositoryControllerUpdateTests, IRepositoryControllerDeleteTests, IRepositoryControllerGetTests
+    public interface IRepositoryControllerTests<TEntity> : 
+        IRepositoryControllerCreateTests<TEntity>, 
+        IRepositoryControllerUpdateTests<TEntity>,
+        IRepositoryControllerDeleteTests<TEntity>,
+        IRepositoryControllerGetTests<TEntity>
     {
 
     }
-    public interface IRepositoryControllerCreateTests
+    public interface IRepositoryControllerCreateTests<TEntity>
     {
-        void CreatesSuccessfully();
-        void ReportsValidationErrorsWhenCreating();
+        void CreatesSuccessfully(TEntity entity);
+        void ReportsValidationErrorsWhenCreating(TEntity entity, string[] expectedErrors);
     }
-    public interface IRepositoryControllerUpdateTests
+    public interface IRepositoryControllerUpdateTests<TEntity>
     {
-        void UpdatesSuccessfully();
-        void ReportsValidationErrorsWhenUpdating();
+        void UpdatesSuccessfully(TEntity entity);
+        void ReportsValidationErrorsWhenUpdating(TEntity entity, string[] expectedErrors);
     }
-    public interface IRepositoryControllerDeleteTests
+    public interface IRepositoryControllerDeleteTests<TEntity>
     {
-        void DeletesSuccessfully();
-        void ReportsValidationErrorsWhenDeleting();
+        void DeletesSuccessfully(TEntity entity);
+        void DeletesByIdSuccessfully(Guid guid);
+        void FailsDeletionIfIdDoesNotExist(Guid guid);
     }
-    public interface IRepositoryControllerGetTests
+    public interface IRepositoryControllerGetTests<TEntity>
     {
-        void DeletesSuccessfully();
-        void ReportsValidationErrorsWhenDeleting();
+        void GetsAllRecordsSuccessfully(int expectedNumberOfRecords);
+        void GetsSpecifiedNumberOfRecordsAtMax(int numberOfRecords);
+        void GetsSpecifiedNumberOfPagesAndRecords(int pageSize, int numberOfPages);
     }
 }
