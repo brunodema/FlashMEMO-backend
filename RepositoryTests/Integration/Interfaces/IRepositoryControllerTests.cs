2b9c5b50411e4f7c8e78ec00be365dfe5f410aad
Bruno Demantova <brunoeckdema@gmail.com>
2021-06-06 14:15:20 -0300

    start implementing test interfaces

---
+++ RepositoryTests/Integration/Interfaces/IRepositoryControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Integration.Interfaces
+{
+    public interface IRepositoryControllerTests : IRepositoryControllerCreateTests, IRepositoryControllerUpdateTests, IRepositoryControllerDeleteTests, IRepositoryControllerGetTests
+    {
+
+    }
+    public interface IRepositoryControllerCreateTests
+    {
+        void CreatesSuccessfully();
+        void ReportsValidationErrorsWhenCreating();
+    }
+    public interface IRepositoryControllerUpdateTests
+    {
+        void UpdatesSuccessfully();
+        void ReportsValidationErrorsWhenUpdating();
+    }
+    public interface IRepositoryControllerDeleteTests
+    {
+        void DeletesSuccessfully();
+        void ReportsValidationErrorsWhenDeleting();
+    }
+    public interface IRepositoryControllerGetTests
+    {
+        void DeletesSuccessfully();
+        void ReportsValidationErrorsWhenDeleting();
+    }
+}
