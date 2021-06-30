b3b082e0b86c3146e4dcc80a6c1d1efd30ef353d
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2021-06-29 22:00:09 -0300

    add default values for properties

--- Data/Models/News.cs
+++ Data/Models/News.cs
@@ -7,14 +7,15 @@ namespace Data.Models
 {
     public class News : INews
     {
+        // kids, ALWAYS set default values for properties in the database. This will avoid errors when dealing with ICollection items + LINQ, especially for cases when the current member of the lambda being analyzed is a "null" item. 
         public News() { }
         public Guid NewsID { get; set; }
-        public string Title { get; set; }
-        public string Subtitle { get; set; }
-        public string ThumbnailPath { get; set; }
-        public string Content { get; set; }
-        public DateTime CreationDate { get; set; }
-        public DateTime LastUpdated { get; set; }
+        public string Title { get; set; } = "";
+        public string Subtitle { get; set; } = "";
+        public string ThumbnailPath { get; set; } = "";
+        public string Content { get; set; } = "";
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime LastUpdated { get; set; } = DateTime.Now;
 
         public Guid GetId()
         {
