17ceb2fbf08803bb1e9d1ef389703a688cd2c60e
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-01-25 23:38:22 -0300

    add common interface for API controllers

---
+++ API/Controllers/Abstract/GenericAPIController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace API.Controllers.Abstract
+{
+    // I was gonna add a 'ValidateInput' function here too, but I realized that some APIs might require more than one validation function. Even though this likely won't happen, I don't want to overcomplicate things for the moment.
+    public abstract class GenericAPIController : ControllerBase
+    {
+        /// <summary>
+        /// Test if the API is reachable by the client.
+        /// </summary>
+        /// <returns>An HTTP response object. Not sure if this is the most appropriate class to be returned, but the most important thing is to check if a 200 will be returned or not. </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        protected virtual HttpResponse CheckAvailability()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Asks the API provider for information on the comsumption of the API in a given time period. Ex: Google APIs have request limits, especially the free tiers of them.
+        /// </summary>
+        /// <returns>An HTTP response object. Not sure if this is the most appropriate class to be returned, but the most important thing is to check if the API has some sort of monitoring or not.</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        protected virtual HttpResponse CheckPeriodComsumption()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
