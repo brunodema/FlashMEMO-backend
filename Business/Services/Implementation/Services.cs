7ca804b48ec5d6edfcf6015a8d08d1e9a0a4b670
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-01-26 03:41:34 -0300

    apply new file structure to Business project

---
+++ Business/Services/Implementation/Services.cs
@@ -0,0 +1,247 @@
+﻿using Business.Services.Abstract;
+using Business.Services.Interfaces;
+using Business.Tools;
+using Business.Tools.Interfaces;
+using Data.Models.Implementation;
+using Data.Repository.Implementation;
+using Google.Apis.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using static Google.Apis.CustomSearchAPI.v1.CseResource;
+using static Google.Apis.CustomSearchAPI.v1.Data.Result;
+
+namespace Business.Services.Implementation
+{
+    public class NewsService : GenericRepositoryService<NewsRepository, Guid, News>
+    {
+        public NewsService(NewsRepository baseRepository, IOptions<GenericRepositoryServiceOptions> serviceOptions) : base(baseRepository, serviceOptions.Value) { }
+        public override ValidatonResult CheckIfEntityIsValid(News entity)
+        {
+            bool areDatesValid = entity.CreationDate <= entity.LastUpdated;
+
+            List<string> errors = new();
+            if (!areDatesValid)
+            {
+                errors.Add("The last updated date must be more recent than the creation date.");
+            }
+
+            return new ValidatonResult 
+            {
+                IsValid = areDatesValid,
+                Errors = errors
+            };
+        }
+    }
+
+    public class JWTServiceOptions : IJWTServiceOptions
+    {
+        public string ValidIssuer { get; set; }
+        public string ValidAudience { get; set; }
+        public double TimeToExpiration { get; set; }
+        public string Secret { get; set; }
+    }
+
+    public class JWTService : IJWTService
+    {
+        private readonly IJWTServiceOptions _options;
+
+        public JWTService(IOptions<JWTServiceOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public string CreateLoginToken(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            foreach (var role in user.UserRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role.RoleId));
+            }
+
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Secret));
+
+            return new JwtSecurityTokenHandler().WriteToken(new JwtSecurityToken(
+                issuer: _options.ValidIssuer, audience: _options.ValidAudience,
+                expires: DateTime.Now.AddSeconds(Convert.ToDouble(_options.TimeToExpiration)),
+                claims: claims,
+                signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+            ));
+
+        }
+    }
+
+    // no interfaces for this one because there are no other viable replacements for this API at the moment
+    public class CustomSearchAPIImageResult
+    {
+        public string Title { get; set; }
+        public ImageData Image { get; set; }
+        public string Link { get; set; }
+
+        public CustomSearchAPIImageResult(string title, ImageData imageData, string link)
+        {
+            Title = title;
+            Image = imageData;
+            Link = link;
+        }
+    }
+
+    public class CustomSearchAPIResponse
+    {
+        public IEnumerable<CustomSearchAPIImageResult> Results { get; set; }
+        public int ResultSize { get; set; }
+        public UInt64 PageIndex { get; set; }
+        public UInt64 TotalAmount { get; set; }
+        public UInt64 TotalPages { get; set; }
+    }
+
+    public class CustomSearchAPIServiceOptions
+    {
+        public string BaseURL { get; set; }
+        public string EngineID { get; set; }
+        public string Token { get; set; }
+    }
+    public class CustomSearchAPIService : IAPIService
+    {
+        private readonly CustomSearchAPIServiceOptions _options;
+
+        public CustomSearchAPIService(IOptions<CustomSearchAPIServiceOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public HttpResponse CheckAvailability()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpResponse CheckPeriodComsumption()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsSearchTextValid(string searchText)
+        {
+            return !String.IsNullOrEmpty(searchText);
+        }
+
+        private List<string> IsInputValid(string searchText, int pageSize, int pageNumber)
+        {
+            var errorMessages = new List<string>();
+
+            if (!IsSearchTextValid(searchText))
+            {
+                errorMessages.Add("The search text used is not valid. It is either 'null' or empty.");
+            }
+            if (pageSize <= 0 || pageSize > 10) // the GT part is a restriction of the Google API itself
+            {
+                errorMessages.Add("The page size has an invalid number (less or equal than 0, or greater than 10).");
+            }
+            if (pageNumber <= 0)
+            {
+                errorMessages.Add("The page size has an invalid number (less or equal than 0).");
+            }
+
+            return errorMessages;
+        }
+
+        public async Task<CustomSearchAPIResponse> Search(string searchText, int pageSize = 10, int pageNumber = 1)
+        {
+            try
+            {
+                using (var service = new Google.Apis.CustomSearchAPI.v1.CustomSearchAPIService(new BaseClientService.Initializer { ApplicationName = "FlashMEMO Image Search", ApiKey = _options.Token }))
+                {
+                    ListRequest listRequest = service.Cse.List();
+
+                    var validationsMessages = IsInputValid(searchText, pageSize, pageNumber);
+                    if (validationsMessages.Count > 0)
+                    {
+                        throw new InputValidationException()
+                        {
+                            InputValidationErrors = validationsMessages
+                        };
+                    }
+
+                    listRequest.Q = searchText;
+                    listRequest.SearchType = ListRequest.SearchTypeEnum.Image;
+                    listRequest.Cx = _options.EngineID;
+                    listRequest.Start = (pageSize * pageNumber) - pageSize;
+                    listRequest.Num = pageSize;
+                    var results = await listRequest.ExecuteAsync();
+
+                    var totalAmount = Convert.ToUInt64(results?.SearchInformation.TotalResults ?? "0");
+                    return new CustomSearchAPIResponse
+                    {
+                        Results = results.Items.Select(i => new CustomSearchAPIImageResult(i.Title, i.Image, i.Link)).ToList(),
+                        ResultSize = results?.Items?.Count ?? 0,
+                        PageIndex = Convert.ToUInt64(pageNumber),
+                        TotalAmount = totalAmount,
+                        TotalPages = Convert.ToUInt64(Math.Ceiling(totalAmount / (double)pageSize))
+
+                    };
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+    public class AuthServiceOptions : IAuthServiceOptions
+    {
+
+    }
+    public class AuthService : IAuthService
+    {
+        private readonly IAuthServiceOptions _options;
+        private readonly ApplicationUserRepository _applicationUserRepository;
+        private readonly RoleRepository _roleRepository;
+
+        public AuthService(IOptions<AuthServiceOptions> options, ApplicationUserRepository applicationUserRepository, RoleRepository roleRepository)
+        {
+            _options = options.Value;
+            _applicationUserRepository = applicationUserRepository;
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<bool> AreCredentialsValidAsync(ICredentials credentials)
+        {
+            if (await UserAlreadyExistsAsync(credentials.Email))
+            {
+                if (await GetUserByEmailAndCheckCredentialsAsync(credentials)) return true;
+            }
+
+            return false;
+        }
+
+        public async Task<IdentityResult> CreateUserAsync(ApplicationUser user, string cleanPassword)
+        {
+            var result = await _applicationUserRepository.CreateAsync(user);
+            await _applicationUserRepository.SetInitialPassword(user.Id, cleanPassword);
+            return result;
+        }
+
+        public async Task<bool> UserAlreadyExistsAsync(string email)
+        {
+            return (await _applicationUserRepository.SearchFirstAsync(u => u.Email == email)) != null;
+        }
+        public async Task<bool> GetUserByEmailAndCheckCredentialsAsync(ICredentials credentials)
+        {
+            var user = await _applicationUserRepository.SearchFirstAsync(u => u.Email == credentials.Email);
+            return await _applicationUserRepository.CheckPasswordAsync(user.Id, credentials.PasswordHash);
+        }
+    }
+}
---
+++ Business/Services/Interfaces/ServiceInterfaces.cs
@@ -0,0 +1,77 @@
+﻿using Business.Tools;
+using Business.Tools.Interfaces;
+using Data.Models.Implementation;
+using Data.Tools.Implementation;
+using Data.Tools.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Business.Services.Interfaces
+{
+    public interface IRepositoryService<TEntity, TKey>
+        where TEntity : class
+    {
+        public Task<bool> IdAlreadyExists(TKey id);
+        public Task CreateAsync(TEntity entity, object[] auxParams = null); // to cover the 'CreateUserAsync' case (requires password)
+        public Task UpdateAsync(TEntity entity);
+        public Task<TEntity> GetbyIdAsync(TKey id);
+        public IEnumerable<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, GenericSortOptions<TEntity> sortOptions, int numRecords = 1000);
+        public Task RemoveByIdAsync(TKey guid);
+        public IEnumerable<TEntity> SearchAndOrder(IQueryFilterOptions<TEntity> filterOptions, GenericSortOptions<TEntity> sortOptions); // probably will transition towards this one
+        public ValidatonResult CheckIfEntityIsValid(TEntity entity);
+    }
+
+    public interface IJWTServiceOptions
+    {
+        string ValidIssuer { get; set; }
+        string ValidAudience { get; set; }
+        double TimeToExpiration { get; set; }
+        string Secret { get; set; }
+    }
+    public interface IJWTService
+    {
+        public string CreateLoginToken(ApplicationUser user);
+    }
+
+    public interface IDictionaryAPIServiceOptions
+    {
+        string BaseURL { get; set; }
+        string Token { get; set; }
+    }
+    public class DictionaryAPIService
+    {
+
+    }
+
+    public interface IAuthServiceOptions
+    {
+    }
+    public interface IAuthService
+    {
+        public Task<bool> UserAlreadyExistsAsync(string email);
+        public Task<IdentityResult> CreateUserAsync(ApplicationUser user, string cleanPassword);
+        public Task<bool> AreCredentialsValidAsync(ICredentials credentials);
+    }
+
+    // I was gonna add a 'ValidateInput' function here too, but I realized that some APIs might require more than one validation function. Even though this likely won't happen, I don't want to overcomplicate things for the moment.
+    public interface IAPIService
+    {
+        /// <summary>
+        /// Test if the API is reachable by the client.
+        /// </summary>
+        /// <returns>An HTTP response object. Not sure if this is the most appropriate class to be returned, but the most important thing is to check if a 200 will be returned or not. </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        HttpResponse CheckAvailability();
+
+        /// <summary>
+        /// Asks the API provider for information on the comsumption of the API in a given time period. Ex: Google APIs have request limits, especially the free tiers of them.
+        /// </summary>
+        /// <returns>An HTTP response object. Not sure if this is the most appropriate class to be returned, but the most important thing is to check if the API has some sort of monitoring or not.</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        HttpResponse CheckPeriodComsumption();
+    }
+}
