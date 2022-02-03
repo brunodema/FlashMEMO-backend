6e2739cfd43d1536ad515aa8768831cd5c25ad3c
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-02-03 20:55:11 -0300

    add new interface prototypes

--- Data/Models/Interfaces/ModelInterfaces.cs
+++ Data/Models/Interfaces/ModelInterfaces.cs
@@ -1,5 +1,7 @@
-﻿using Data.Repository.Interfaces;
+﻿using Data.Models.Implementation;
+using Data.Repository.Interfaces;
 using System;
+using System.Collections.Generic;
 
 namespace Data.Models.Interfaces
 {
@@ -12,5 +14,31 @@ namespace Data.Models.Interfaces
         public string Content { get; set; }
         public DateTime CreationDate { get; set; }
         public DateTime LastUpdated { get; set; }
+    }
+
+    // Both Decks and Flashcards will have simplistic implementations for the moment, since several issues will arise from the initial coding.
+    public interface IDeck : IDatabaseItem<Guid>
+    {
+        public Guid DeckID { get; set; }
+        public IEnumerable<IFlashcard> Flashcards { get; set; }
+        public ApplicationUser Owner { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public interface IFlashcard : IDatabaseItem<Guid>
+    {
+        public Guid FlashcardID { get; set; }
+        public ApplicationUser Owner { get; set; }
+
+        public int Level { get; set; }
+        public string FrontContent { get; set; }
+        public string BackContent { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
