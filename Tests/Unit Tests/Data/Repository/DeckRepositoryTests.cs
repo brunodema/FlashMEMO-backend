23794c2f457c3af73da67bfcef4c0b6bb75b6e87
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-02-03 21:48:04 -0300

    add fixture class to unit test (might not be necessary)

--- Tests/Unit Tests/Data/Repository/DeckRepositoryTests.cs
+++ Tests/Unit Tests/Data/Repository/DeckRepositoryTests.cs
@@ -3,20 +3,23 @@ using Data.Models.Implementation;
 using Data.Repository.Implementation;
 using Data.Repository.Interfaces;
 using System;
+using Tests.Integration.Fixtures;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Tests.Unit_Tests.Data.Repository
 {
-    public class DeckRepositoryTests
+    public class DeckRepositoryTests : IClassFixture<IntegrationTestFixture>
     {
         protected readonly ITestOutputHelper _output;
         protected readonly DeckRepository _repository;
+        protected readonly IntegrationTestFixture _fixture;
 
-        public DeckRepositoryTests(ITestOutputHelper output, DeckRepository deckRepository)
+        public DeckRepositoryTests(IntegrationTestFixture integrationTestFixture, ITestOutputHelper output, DeckRepository deckRepository)
         {
             _output = output;
             _repository = deckRepository;
+            _fixture = integrationTestFixture;
         }
 
         [Fact]
