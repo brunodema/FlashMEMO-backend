9aa0397b5eef3b543e730bb758dae703e6581045
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-02-04 21:55:36 -0300

    start implementation of an interface for repository unit testing

--- Tests/Unit Tests/Data/Repository/DeckRepositoryTests.cs
+++ Tests/Unit Tests/Data/Repository/DeckRepositoryTests.cs
@@ -1,62 +1,68 @@
 ﻿using Data.Context;
 using Data.Models.Implementation;
+using Data.Repository.Abstract;
 using Data.Repository.Implementation;
+using Data.Repository.Interfaces;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using Tests.Integration.Fixtures;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Tests.Unit_Tests.Data.Repository
 {
-    public class DeckRepositoryTests : IDisposable
+    public abstract class GenericRepositoryUnitTests<RepositoryClass, TEntity, TKey>  : IDisposable
+        where RepositoryClass : GenericRepository<TEntity, TKey, FlashMEMOContext>
+        where TEntity : class, IDatabaseItem<TKey>
     {
         protected readonly ITestOutputHelper _output;
-        protected readonly DeckRepository _repository;
+        protected readonly RepositoryClass _repository;
         protected readonly FlashMEMOContext _context;
 
-        public DeckRepositoryTests(ITestOutputHelper output)
+        public GenericRepositoryUnitTests(ITestOutputHelper output, RepositoryClass repositoryClass)
         {
             _output = output;
-
-            var options = new DbContextOptionsBuilder<FlashMEMOContext>().UseInMemoryDatabase(databaseName: "FlashMEMOTest").Options;
-            _context = new FlashMEMOContext(options);
-
-            _repository = new DeckRepository(_context);
+            _repository = repositoryClass;
         }
 
-        [Fact]
-        public async void test()
+        public async virtual void CreateEntity(TEntity entity)
         {
-            var deck = new Deck()
-            {
-                DeckID = new Guid(),
-                Name = "test deck",
-                Description = "this is a test deck",
-            };
+            // Arrange
+            // Act
+            await _repository.CreateAsync(entity);
+            var entityFromRepository = await _repository.GetByIdAsync(entity.GetId());
 
-            var flashcard = new Flashcard()
-            {
-                FrontContent = "nothing in front",
-                BackContent = "nothing in back",
-                Level = 1,
-            };
-
-            await _repository.CreateAsync(deck);
-
-            var deckFromRepo = await _repository.GetByIdAsync(deck.DeckID);
-            deck.Flashcards = new List<Flashcard> { flashcard };
-
-            await _repository.SaveChangesAsync();
-
-            deckFromRepo = await _repository.GetByIdAsync(deck.DeckID);
+            // Assert
+            entity.Should().Be(entityFromRepository);
         }
 
         public void Dispose()
         {
-            _repository.Dispose();
+            _repository?.Dispose();
             _context?.Dispose();
+        }
+        //public virtual void ReadEntity() { }
+        //public virtual void UpdateEntity() { }
+        //public virtual void DeleteEntity() { }
+    }
+
+    public class DeckRepositoryUnitTests : GenericRepositoryUnitTests<DeckRepository, Deck, Guid>
+    {
+        // Yes, the base constructor is atrocius at the moment, but I couldn't find a better way the repository class
+        public DeckRepositoryUnitTests(ITestOutputHelper output) : base(output, new DeckRepository(new FlashMEMOContext(new DbContextOptionsBuilder<FlashMEMOContext>().UseInMemoryDatabase(databaseName: "FlashMEMOTest").Options))) { }
+
+        public static IEnumerable<object[]> CreateEntityData =>
+            new List<object[]>
+            {
+                new object[] { new Deck { Name = "test", Description = "this is a test deck" } },
+                new object[] { new Deck { Name = "test number two", Description = "this is another test deck" } }
+            };
+
+        [Theory, MemberData(nameof(CreateEntityData))]
+        public override void CreateEntity(Deck entity)
+        {
+            base.CreateEntity(entity);
         }
     }
 }
