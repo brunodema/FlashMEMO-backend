23af8f1aa5b4c0274ecffae2eb05902c25781c2d
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-02-05 02:51:41 -0300

    tiny refactor

--- Tests/Unit Tests/Data/Repository/RepositoryTests.cs
+++ Tests/Unit Tests/Data/Repository/RepositoryTests.cs
@@ -24,6 +24,7 @@ namespace Tests.Unit_Tests.Data.Repository
         {
             // terrible design since context and repository are used throughout the class, but only set on the inherited class. Unfortunately, there is no easy/compact way to do this using fixtures (no time to pass to a member of the child class, resulting in the temporary object being disposed) or other methods (can not initialize repository class here since it uses a constructor with parameters).
             _output = output;
+            _context = new FlashMEMOContext(new DbContextOptionsBuilder<FlashMEMOContext>().UseInMemoryDatabase(databaseName: "FlashMEMOTest").Options);
         }
 
         public async virtual void CreateEntity(TEntity entity)
@@ -52,7 +53,6 @@ namespace Tests.Unit_Tests.Data.Repository
     {
         public DeckRepositoryUnitTests(ITestOutputHelper output) : base(output)
         { 
-            _context = new FlashMEMOContext(new DbContextOptionsBuilder<FlashMEMOContext>().UseInMemoryDatabase(databaseName: "FlashMEMOTest").Options);
             _repository = new DeckRepository(_context);
         }
 
@@ -70,26 +70,25 @@ namespace Tests.Unit_Tests.Data.Repository
         }
     }
 
-    //// this class is here just to prove if the concept of the generic class works or not for multiple types
-    //public class NewsRepositoryUnitTests : GenericRepositoryUnitTests<NewsRepository, News, Guid>
-    //{
-    //    public NewsRepositoryUnitTests(ITestOutputHelper output) : base(output)
-    //    {
-    //        _context = new FlashMEMOContext(new DbContextOptionsBuilder<FlashMEMOContext>().UseInMemoryDatabase(databaseName: "FlashMEMOTest").Options);
-    //        _repository = new NewsRepository(_context);
-    //    }
+    // this class is here just to prove if the concept of the generic class works or not for multiple types
+    public class NewsRepositoryUnitTests : GenericRepositoryUnitTests<NewsRepository, News, Guid>
+    {
+        public NewsRepositoryUnitTests(ITestOutputHelper output) : base(output)
+        {
+            _repository = new NewsRepository(_context);
+        }
 
-    //    public static IEnumerable<object[]> CreateEntityData =>
-    //        new List<object[]>
-    //        {
-    //            new object[] { new News { Title = "title", Subtitle = "subtitle", Content = "content" } },
-    //            new object[] { new News { Title = "title1", Subtitle = "subtitle1", Content = "content1" } },
-    //        };
+        public static IEnumerable<object[]> CreateEntityData =>
+            new List<object[]>
+            {
+                new object[] { new News { Title = "title", Subtitle = "subtitle", Content = "content" } },
+                new object[] { new News { Title = "title1", Subtitle = "subtitle1", Content = "content1" } },
+            };
 
-    //    [Theory, MemberData(nameof(CreateEntityData))]
-    //    public override void CreateEntity(News entity)
-    //    {
-    //        base.CreateEntity(entity);
-    //    }
-    //}
+        [Theory, MemberData(nameof(CreateEntityData))]
+        public override void CreateEntity(News entity)
+        {
+            base.CreateEntity(entity);
+        }
+    }
 }
