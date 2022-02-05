8da9b81069f909fb670b93bb65f128617bd95cbe
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-02-05 01:00:16 -0300

    simplify implementation

--- Tests/Unit Tests/Data/Repository/DeckRepositoryTests.cs
+++ Tests/Unit Tests/Data/Repository/DeckRepositoryTests.cs
@@ -20,12 +20,6 @@ namespace Tests.Unit_Tests.Data.Repository
         protected RepositoryClass _repository;
         protected FlashMEMOContext _context;
 
-        /// <summary>
-        /// Retrieves the database source for the entity associated with the tests. Ex: 'NewsRepository' should retrieve the 'News' DbSet present in the context class, so validations for the reopsitory methods can be cross-checked with functions from the context class.
-        /// </summary>
-        /// <returns></returns>
-        public abstract DbSet<TEntity> GetDbSet();
-
         public GenericRepositoryUnitTests(ITestOutputHelper output)
         {
             // terrible design since context and repository are used throughout the class, but only set on the inherited class. Unfortunately, there is no easy/compact way to do this using fixtures (no time to pass to a member of the child class, resulting in the temporary object being disposed) or other methods (can not initialize repository class here since it uses a constructor with parameters).
@@ -37,7 +31,7 @@ namespace Tests.Unit_Tests.Data.Repository
             // Arrange
             // Act
             await _repository.CreateAsync(entity);
-            var entityFromRepository = GetDbSet().Find(entity.GetId());
+            var entityFromRepository = _context.Set<TEntity>().Find(entity.GetId());
 
             // Assert
             entity.Should().Be(entityFromRepository);
@@ -60,11 +54,6 @@ namespace Tests.Unit_Tests.Data.Repository
         { 
             _context = new FlashMEMOContext(new DbContextOptionsBuilder<FlashMEMOContext>().UseInMemoryDatabase(databaseName: "FlashMEMOTest").Options);
             _repository = new DeckRepository(_context);
-        }
-
-        public override DbSet<Deck> GetDbSet()
-        {
-            return _context.Decks;
         }
 
         public static IEnumerable<object[]> CreateEntityData =>
