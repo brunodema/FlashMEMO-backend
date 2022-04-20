e6383445fa4d789a3ce3a5f7f6634e9b5d4e91ff
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-20 20:29:23 -0300

    add insightful comment

--- Business/Services/Implementation/Services.cs
+++ Business/Services/Implementation/Services.cs
@@ -53,7 +53,7 @@ namespace Business.Services.Implementation
             if (!_languageService.LanguageExists(entity.Language?.ISOCode ?? null)) errors.Add("The language code provided is not valid.");
             if(!_authService.UserAlreadyExistsAsync(entity.Owner?.Email ?? null).Result) errors.Add($"The user owner does not seem to exist within FlashMEMO.");
 
-            return new ValidatonResult() { IsValid = errors.Count == 0, Errors = errors };
+            return new ValidatonResult() { IsValid = errors.Count == 0, Errors = errors }; // dummy function for now
         }
     }
 
@@ -63,7 +63,7 @@ namespace Business.Services.Implementation
 
         public override ValidatonResult CheckIfEntityIsValid(Flashcard entity)
         {
-            return new ValidatonResult() { IsValid = true }; // dummy function for now
+            return new ValidatonResult() { IsValid = true }; // I actually went through this one, and saw that there is no immediate need to implement complex checks for Flashcard objects. Yes, there are some properties which could profit from some extra logic (ex: 'Content' columns should match the chosen layout, 'Level' should not be negative, date checks, etc), but adding that would be distracting right now.
         }
     }
 
