0f2ba4d119343f27eb9de9d2d6fef42e61ecaeed
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-04-22 12:22:11 -0300

    set default GUID to empty isntead of new()

--- Data/Models/Implementation/Models.cs
+++ Data/Models/Implementation/Models.cs
@@ -12,7 +12,7 @@ namespace Data.Models.Implementation
         // kids, ALWAYS set default values for properties in the database. This will avoid errors when dealing with ICollection items + LINQ, especially for cases when the current member of the lambda being analyzed is a "null" item. 
         public News() { }
 
-        public Guid NewsID { get; set; } = new Guid();
+        public Guid NewsID { get; set; } = Guid.Empty;
 
         public string Title { get; set; } = "";
         public string Subtitle { get; set; } = "";
@@ -29,7 +29,7 @@ namespace Data.Models.Implementation
     {
         public Deck() { }
 
-        public Guid DeckID { get; set; } = new Guid();
+        public Guid DeckID { get; set; } = Guid.Empty;
 
         public List<Flashcard> Flashcards { get; set; } = new List<Flashcard>();
         public ApplicationUser Owner { get; set; } = null;
@@ -50,7 +50,7 @@ namespace Data.Models.Implementation
     {
         public Flashcard() { }
 
-        public Guid FlashcardID { get; set; } = new Guid();
+        public Guid FlashcardID { get; set; } = Guid.Empty;
 
         public Deck Deck { get; set; } = null;
         public Guid DeckId { get; set; } = Guid.Empty;
