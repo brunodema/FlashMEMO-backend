fc6445fd50b1ff958d0425005d2bb3d45eb052ce
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-18 15:27:48 -0300

    add repository tests for User (preparation for cascade deletion tests)

--- Tests/Tests/Unit Tests/Data/Repository/RepositoryTests.cs
+++ Tests/Tests/Unit Tests/Data/Repository/RepositoryTests.cs
@@ -5,6 +5,8 @@ using Data.Repository.Implementation;
 using Data.Repository.Interfaces;
 using Data.Tools.Sorting;
 using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -30,9 +32,11 @@ namespace Tests.Unit_Tests.Data.Repository
 
         public GenericRepositoryUnitTests(ITestOutputHelper output)
         {
-            //terrible design since context and repository are used throughout the class, but only set on the inherited class. Unfortunately, there is no easy/compact way to do this using fixtures(no time to pass to a member of the child class, resulting in the temporary object being disposed) or other methods(can not initialize repository class here since it uses a constructor with parameters).
+            // Terrible design since context and repository are used throughout the class, but only set on the inherited class. Unfortunately, there is no easy/compact way to do this using fixtures(no time to pass to a member of the child class, resulting in the temporary object being disposed) or other methods(can not initialize repository class here since it uses a constructor with parameters).
 
-            // maybe it would work if the repository classes were declared like Repository<News> or Repository<Deck>, so something like _repository = new Repository<TEntity>(_context) could be used? - The problem from doing this is that this would restrict the child class from running its own tests (i.e., NewsRepository might want to test functions specific to it). 
+            // Maybe it would work if the repository classes were declared like Repository<News> or Repository<Deck>, so something like _repository = new Repository<TEntity>(_context) could be used? - The problem from doing this is that this would restrict the child class from running its own tests (i.e., NewsRepository might want to test functions specific to it). 
+
+            // (18/06/2022) It's been so long since I actually checked the comments above that they might as well be completely wrong at this point. FACT: I could use DI just as I use it for controller tests (I didn't implement this here because I didn't know how to at the time), and that would make several things easier.
             _output = output;
             _context = new FlashMEMOContext(new DbContextOptionsBuilder<FlashMEMOContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options, Options.Create(new FlashMEMOContextOptions { SeederPath = "../../../../Data/Seeder", DefaultUserPassword = "Default@Password123" }));
         }
@@ -42,7 +46,7 @@ namespace Tests.Unit_Tests.Data.Repository
         /// </summary>
         /// <param name="source"></param>
         /// <param name="destination"></param>
-        protected void SafeEFEntityValueCopy(TEntity source, TEntity destination)
+        protected virtual void SafeEFEntityValueCopy(TEntity source, TEntity destination)
         {
             source.DbId = destination.DbId; // makes assignment here so the error is not thrown below (i.e., Id gets set to a value different than before)
             _context.Entry(destination).CurrentValues.SetValues(source);
@@ -333,7 +337,6 @@ namespace Tests.Unit_Tests.Data.Repository
         }
     }
 
-    // this class is here just to prove if the concept of the generic class works or not for multiple types
     public class NewsRepositoryUnitTests : GenericRepositoryUnitTests<News, Guid>
     {
         public NewsRepositoryUnitTests(ITestOutputHelper output) : base(output)
@@ -590,4 +593,139 @@ namespace Tests.Unit_Tests.Data.Repository
         }
     }
     #endregion
+
+    public class UserRepositoryUnitTests : GenericRepositoryUnitTests<User, string>
+    {
+        public UserRepositoryUnitTests(ITestOutputHelper output) : base(output)
+        {
+            // stole this implementation from what was being used in the now deprecated UserRepository tests, which seems to be taken from here: https://stackoverflow.com/questions/38253607/creating-a-usermanager-outside-of-built-in-dependency-injection-system
+            var userManager = new UserManager<User>(
+                new UserStore<User>(_context),
+                null,
+                new PasswordHasher<User>(),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+            _repository = new UserRepository(_context, userManager);
+
+            AssertionOptions.AssertEquivalencyUsing(o => o.Excluding(p => p.Name.ToLower().Contains("concurrencystamp"))); // This is necessary so 'ConcurrencyStamp' properties don't throw useless comparison exceptions during testing the Identity classes.
+        }
+
+         // I changed the implementation a bit here to reduce code duplication
+
+        public static IEnumerable<object[]> CreateAndReadEntityData =>
+            new List<object[]>
+            {
+                new object[] { TestUser1 },
+                new object[] { TestUser2 },
+            };
+
+        [Theory, MemberData(nameof(CreateAndReadEntityData))]
+        public override void CreateEntity(User entity)
+        {
+            base.CreateEntity(entity);
+        }
+
+        [Theory, MemberData(nameof(CreateAndReadEntityData))]
+        public override void ReadEntity(User entity)
+        {
+            base.ReadEntity(entity);
+        }
+
+        public static IEnumerable<object[]> UpdateEntityData =>
+        new List<object[]>
+        {
+                new object[] {TestUser1, new User() { Name = "newName", Surname = "newSurname" } },
+        };
+
+        /// <summary>
+        /// I override this method so I can alter the way stamp properties are handled during the value copy, since mismatched values end up throwing concurrency exceptions by EF Core.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        protected override void SafeEFEntityValueCopy(User source, User destination)
+        {
+            source.DbId = destination.DbId;
+            source.ConcurrencyStamp = destination.ConcurrencyStamp;
+
+            _context.Entry(destination).CurrentValues.SetValues(source);
+        }
+
+        [Theory, MemberData(nameof(UpdateEntityData))]
+        public override void UpdateEntity(User previousEntity, User updatedEntity)
+        {
+            base.UpdateEntity(previousEntity, updatedEntity);
+        }
+
+        public static IEnumerable<object[]> DeleteEntityData =>
+        new List<object[]>
+        {
+                new object[] { TestUser1 },
+                new object[] { TestUser2 },
+        };
+
+        [Theory, MemberData(nameof(DeleteEntityData))]
+        public override void DeleteEntity(User entity)
+        {
+            base.DeleteEntity(entity);
+        }
+
+        // specific test data
+        private static User TestUser1 = new User { Email = "test@email.com", NormalizedEmail = "test@email.com", Name = "test", Surname = "user", UserName = "testuser", NormalizedUserName = "testuser" };
+        private static User TestUser2 = new User { Email = "test2@email.com", NormalizedEmail = "test2@email.com", Name = "test2", Surname = "user", UserName = "testuser2", NormalizedUserName = "testuser2" };
+        private static User TestUser3 = new User { Email = "test3@email.com", NormalizedEmail = "test3@email.com", Name = "test3", Surname = "user", UserName = "testuser3", NormalizedUserName = "testuser3" };
+        private static User TestUser4 = new User { Email = "test4@email.com", NormalizedEmail = "test4@email.com", Name = "test4", Surname = "user", UserName = "testuser4", NormalizedUserName = "testuser4" };
+        private static User TestUser5 = new User { Email = "test5@email.com", NormalizedEmail = "test5@email.com", Name = "test5", Surname = "user", UserName = "testuser5", NormalizedUserName = "testuser5" };
+
+        private static readonly List<User> FullEntityList = new() { TestUser1, TestUser2, TestUser3, TestUser4, TestUser5 };
+
+        public static IEnumerable<object[]> GetAllEntityData =>
+        new List<object[]>
+        {
+                new object[] { FullEntityList.ToArray() }, // full list
+                new object[] { new User[] { TestUser1, TestUser2, TestUser3 } }, // only some
+                new object[] { new User[] { } } // nothing
+        };
+
+        [Theory, MemberData(nameof(GetAllEntityData))]
+        public override void GetAll(User[] users)
+        {
+            base.GetAll(users);
+        }
+
+        public static IEnumerable<object[]> SearchAndOrder_ValidateOrderingData =>
+        new List<object[]>
+        {
+                new object[] { new List<User>(FullEntityList), new UserSortOptions(SortType.Ascending, "email") },
+                new object[] { new List<User>(FullEntityList), new UserSortOptions(SortType.Descending, "email") },
+                new object[] { new List<User>(FullEntityList), new UserSortOptions(SortType.Ascending, "username") },
+                new object[] { new List<User>(FullEntityList), new UserSortOptions(SortType.Descending, "username") },
+                new object[] { new List<User>(FullEntityList), new UserSortOptions(SortType.Ascending, "default") }, // should go to 'email'
+                new object[] { new List<User>(FullEntityList), new UserSortOptions(SortType.Descending, "default") }, // should go to 'email'
+        };
+
+        [Theory, MemberData(nameof(SearchAndOrder_ValidateOrderingData))]
+        public override void SearchAndOrder_ValidateOrdering(List<User> entities, GenericSortOptions<User> sortOptions)
+        {
+            base.SearchAndOrder_ValidateOrdering(entities, sortOptions);
+        }
+
+        public static IEnumerable<object[]> SearchAndOrder_ValidateFilteringData =>
+        new List<object[]>
+        {
+            new object[] { new ValidateFilteringTestData<User> { entities = FullEntityList, predicate = _ => true } },
+            new object[] { new ValidateFilteringTestData<User> { entities = FullEntityList, predicate = e => e.Email == "test@email.com" } },
+            new object[] { new ValidateFilteringTestData<User> { entities = FullEntityList, predicate = e => e.UserName == "testuser2" } },
+            new object[] { new ValidateFilteringTestData<User> { entities = FullEntityList, predicate = e => e.Email.Contains("email.com") } },
+        };
+
+        [Theory, MemberData(nameof(SearchAndOrder_ValidateFilteringData))]
+        public override void SearchAndOrder_ValidateFiltering(ValidateFilteringTestData<User> testData)
+        {
+            base.SearchAndOrder_ValidateFiltering(testData);
+        }
+    }
 }
