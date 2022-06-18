d81ccb4fab4f4d6e6d5a7df614a1ead80d56eff3
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-18 15:26:01 -0300

    add this just in case I need it in the future

--- Data/Models/Implementation/IdentityModels.cs
+++ Data/Models/Implementation/IdentityModels.cs
@@ -11,16 +11,30 @@ namespace Data.Models.Implementation
     // following implementation follows .NET boilerplate for identity customization (https://docs.microsoft.com/en-us/aspnet/core/security/authentication/customize-identity-model?view=aspnetcore-5.0)
     public class User : IdentityUser, IDatabaseItem<string>
     {
-        public virtual ICollection<UserClaim> Claims { get; set; }
-        public virtual ICollection<UserLogin> Logins { get; set; }
-        public virtual ICollection<UserToken> Tokens { get; set; }
-        public virtual ICollection<UserRole> UserRoles { get; set; }
+        public virtual ICollection<UserClaim> Claims { get; set; } = new List<UserClaim>();
+        public virtual ICollection<UserLogin> Logins { get; set; } = new List<UserLogin>();
+        public virtual ICollection<UserToken> Tokens { get; set; } = new List<UserToken>();
+        public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
 
         public string Name { get; set; } = "";
         public string Surname { get; set; } = "";
 
         [NotMapped]
         public string DbId { get => Id; set => Id = value; }
+
+        //public override bool Equals(object obj)
+        //{
+        //    return obj is User model &&
+        //           Name == model.Name &&
+        //           Surname == model.Surname &&
+        //        NormalizedEmail == model.NormalizedEmail &&
+        //        NormalizedUserName == model.NormalizedUserName;
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return HashCode.Combine(Name, Surname, NormalizedEmail, NormalizedUserName);
+        //}
     }
 
     public class Role : IdentityRole, IDatabaseItem<string>
