bb1277eb8a511cdc69ba247c66dea47ef3074af4
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-21 12:46:58 -0300

    start implementing tests for Auth/JWT

---
+++ Tests/Tests/Integration/Implementation/IdentityControllerTests.cs
@@ -0,0 +1,63 @@
+﻿using API.ViewModels;
+
+namespace Tests.Tests.Integration.Implementation
+{
+    public interface IIdentityControllerTests
+    {
+        // Login tests
+        void SuccessfulLogin(LoginRequestModel request);
+        void FailedLoginWithWrongCredentials(LoginRequestModel request);
+        // JWT tests
+        void SuccessfulTokenRenewal(string expiredAccesstoken, string refreshToken);
+        void FailedTokenRenewalWithInvalidAT(string expiredAccesstoken, string refreshToken);
+        void FailedTokenRenewalWithNotExpiredAT(string expiredAccesstoken, string refreshToken);
+        void FailedTokenRenewalWithInvalidRT(string expiredAccesstoken, string refreshToken);
+        void FailedTokenRenewalWithExpiredRT(string expiredAccesstoken, string refreshToken);
+        void FailedTokenRenewalWithUnmatchedTokens(string expiredAccesstoken, string refreshToken);
+    }
+
+    public abstract class IdentityControllerTests : IIdentityControllerTests
+    {
+        // Login tests
+        public void SuccessfulLogin(LoginRequestModel request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FailedLoginWithWrongCredentials(LoginRequestModel request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // JWT tests
+        public void SuccessfulTokenRenewal(string expiredAccesstoken, string refreshToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FailedTokenRenewalWithInvalidAT(string expiredAccesstoken, string refreshToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FailedTokenRenewalWithNotExpiredAT(string expiredAccesstoken, string refreshToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FailedTokenRenewalWithInvalidRT(string expiredAccesstoken, string refreshToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FailedTokenRenewalWithExpiredRT(string expiredAccesstoken, string refreshToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FailedTokenRenewalWithUnmatchedTokens(string expiredAccesstoken, string refreshToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
