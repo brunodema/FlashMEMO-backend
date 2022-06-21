b28b7313ec5cb1888dc3f60e05de47c285156b1b
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-20 21:19:39 -0300

    move implementation to its own file

---
+++ Business/Services/Implementation/AuthServices.cs
@@ -0,0 +1,62 @@
+﻿using Business.Services.Interfaces;
+using Business.Tools.Interfaces;
+using Data.Models.Implementation;
+using Data.Repository.Implementation;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace Business.Services.Implementation
+{
+    public class AuthServiceOptions : IAuthServiceOptions
+    {
+
+    }
+    public class AuthService : IAuthService<string>
+    {
+        private readonly IAuthServiceOptions _options;
+        private readonly UserRepository _userRepository;
+        private readonly RoleRepository _roleRepository;
+
+        public AuthService(IOptions<AuthServiceOptions> options, UserRepository userRepository, RoleRepository roleRepository)
+        {
+            _options = options.Value;
+            _userRepository = userRepository;
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<User> AreCredentialsValidAsync(IFlashMEMOCredentials credentials)
+        {
+            var user = await _userRepository.GetByUserNameAsync(credentials.Username);
+            if (user != null)
+            {
+                if (await _userRepository.CheckPasswordAsync(user, credentials.Password))
+                    return user;
+            }
+
+            return null;
+        }
+
+        public async Task<string> CreateUserAsync(User user, string cleanPassword)
+        {
+            var result = await _userRepository.CreateAsync(user);
+            await _userRepository.SetInitialPasswordAsync(user, cleanPassword);
+            return result;
+        }
+
+        public async Task<bool> EmailAlreadyRegisteredAsync(string email)
+        {
+            return (await _userRepository.GetByEmailAsync(email)) != null;
+        }
+
+        public async Task<bool> UserExistsAsync(string id)
+        {
+            return (await _userRepository.GetByIdAsync(id)) != null;
+        }
+
+        public async Task<bool> GetUserByUserNameAndCheckCredentialsAsync(IFlashMEMOCredentials credentials)
+        {
+            var user = await _userRepository.GetByUserNameAsync(credentials.Username);
+            return await _userRepository.CheckPasswordAsync(user, credentials.Password);
+        }
+    }
+}
--- Business/Services/Implementation/Services.cs
+++ Business/Services/Implementation/Services.cs
@@ -207,59 +207,6 @@ namespace Business.Services.Implementation
         }
     }
 
-    public class AuthServiceOptions : IAuthServiceOptions
-    {
-
-    }
-    public class AuthService : IAuthService<string>
-    {
-        private readonly IAuthServiceOptions _options;
-        private readonly UserRepository _userRepository;
-        private readonly RoleRepository _roleRepository;
-
-        public AuthService(IOptions<AuthServiceOptions> options, UserRepository userRepository, RoleRepository roleRepository)
-        {
-            _options = options.Value;
-            _userRepository = userRepository;
-            _roleRepository = roleRepository;
-        }
-
-        public async Task<User> AreCredentialsValidAsync(IFlashMEMOCredentials credentials)
-        {
-            var user = await _userRepository.GetByUserNameAsync(credentials.Username);
-            if (user != null)
-            {
-                if (await _userRepository.CheckPasswordAsync(user, credentials.Password))
-                    return user;
-            }
-
-            return null;
-        }
-
-        public async Task<string> CreateUserAsync(User user, string cleanPassword)
-        {
-            var result = await _userRepository.CreateAsync(user);
-            await _userRepository.SetInitialPasswordAsync(user, cleanPassword);
-            return result;
-        }
-
-        public async Task<bool> EmailAlreadyRegisteredAsync(string email)
-        {
-            return (await _userRepository.GetByEmailAsync(email)) != null;
-        }
-
-        public async Task<bool> UserExistsAsync(string id)
-        {
-            return (await _userRepository.GetByIdAsync(id)) != null;
-        }
-
-        public async Task<bool> GetUserByUserNameAndCheckCredentialsAsync(IFlashMEMOCredentials credentials)
-        {
-            var user = await _userRepository.GetByUserNameAsync(credentials.Username);
-            return await _userRepository.CheckPasswordAsync(user, credentials.Password);
-        }
-    }
-
     #region Language Service
     /// <summary>
     /// Service containing auxiliary functions related to languages within FlashMEMO.
