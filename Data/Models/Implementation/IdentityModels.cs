81fe53d686e04424f45d573383a265783c04a140
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-06-30 22:49:06 -0300

    update to nullable

--- Data/Models/Implementation/IdentityModels.cs
+++ Data/Models/Implementation/IdentityModels.cs
@@ -19,7 +19,7 @@ namespace Data.Models.Implementation
 
         public string Name { get; set; } = "";
         public string Surname { get; set; } = "";
-        public DateTime LastLogin { get; set; } = DateTime.MinValue;
+        public DateTime? LastLogin { get; set; } = null;
 
         [JsonIgnore]
         public virtual ICollection<Deck> Decks { get; set; } = new List<Deck>();
