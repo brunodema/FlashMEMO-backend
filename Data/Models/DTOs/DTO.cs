14cbfb664538ba9ec9e4fff0621c946541edc5e5
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-07-12 21:51:39 -0300

    update regex for password

--- Data/Models/DTOs/DTO.cs
+++ Data/Models/DTOs/DTO.cs
@@ -188,7 +188,6 @@ namespace Data.Models.DTOs
         public string Username { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        // regex stolen from here: https://stackoverflow.com/questions/5859632/regular-expression-for-password-validation
         public string Password { get; set; }
 
         public void PassValuesToEntity(User entity)
@@ -205,7 +204,7 @@ namespace Data.Models.DTOs
     {
         public class ValidationMessages
         {
-            public static readonly string InvalidPasswordFormat = "Password must be between 6 and 20 characters and contain one uppercase letter, one lowercase letter, one digit and one special character.";
+            public static readonly string InvalidPasswordFormat = "Password must have at least 8 characters and contain one uppercase letter, one lowercase letter, one digit, and one special character.";
         }
 
         public UserDTOValidator()
@@ -214,7 +213,7 @@ namespace Data.Models.DTOs
             RuleFor(x => x.Surname).NotEmpty();
             RuleFor(x => x.Username).NotEmpty();
             RuleFor(x => x.Email).EmailAddress();
-            RuleFor(x => x.Password).Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$").WithMessage(ValidationMessages.InvalidPasswordFormat);
+            RuleFor(x => x.Password).Matches(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$").WithMessage(ValidationMessages.InvalidPasswordFormat);
         }
     }
 }
