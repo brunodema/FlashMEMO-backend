f6bc8d05b28a1368937f4c666fe958b7da4db086
Bruno Demantova <Bruno_Eck_Dema@hotmail.com>
2022-07-14 15:14:32 -0300

    start implemeting caching service

---
+++ Business/Services/Implementation/CachingService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Services.Implementation
+{
+    public interface ICachingService
+    {
+        Task<byte[]> GetAsync(string key);
+        Task SetAsync(string key, byte[] value);
+        Task RefreshAsync(string key);
+        Task Remove(string key);
+    }
+
+    public class CachingOptions
+    {
+        public string CachingURL { get; set; }
+    }
+
+    public class CachingService
+    {
+        private readonly IDistributedCache _cache;
+        private readonly CachingOptions _options;
+
+        public CachingService(IDistributedCache cache, IOptions<CachingOptions> options)
+        {
+            _cache = cache;
+            _options = options.Value;
+        }
+
+        public Task<byte[]> GetAsync(string key)
+        {
+            return _cache.GetAsync(key);
+        }
+
+        public Task SetAsync(string key, byte[] value)
+        {
+            return _cache.SetAsync(key, value);
+
+        }
+
+        public Task RefreshAsync(string key)
+        {
+            return _cache.RefreshAsync(key);
+
+        }
+
+        public Task Remove(string key)
+        {
+            return _cache.RemoveAsync(key);
+        }
+    }
+}
